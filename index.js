9e013d27c9c2ae853bb49e93eb9ccd0a0c3f6acb
camisev25 <camilasevalho@gmail.com>
2026-02-17 11:20:37 +0000

    Updated according to Ex2.5

--- index.js
+++ index.js
@@ -21,8 +21,49 @@ let top10Films = [
     }
 ];
 
+//Returns all films//
 app.get('/films', (req, res) => {
-    res.json(top10Films);
+    res.send('This will return a list of all movies');
+});
+
+//Returns a single film by title.//
+app.get('/films/:title', (req, res) => {
+    res.send('This will return data about a single movie by title');
+});
+
+//Returns genre info.//
+app.get('/genres/:title', (req, res) => {
+    res.send('This will return data about a genre by name');
+});
+
+//Returns director info.//
+app.get('/directors/:name', (req, res) => {
+    res.send('This will return data about a director by name');
+});
+
+//Registers new user.//
+app.post('/users', (req, res) => {
+    res.send('This will register a new user');
+});
+
+//Update user info//
+app.put('/users/:username', (req, res) => {
+    res.send('This will update user information');
+});
+
+//Adds a film to the user's list of favorites; returns a confirmation message.//
+app.post('/users/:username/films/:filmId', (req,res) => {
+    res.send('This will add a movie to the user’s list of favorites');
+});
+
+//Removes a film from the user's list of favorites; returns a confirmation message.//
+app.delete('/users/:username/films/:filmId', (req, res) => {
+    res.send('This will remove a movie from the user’s list of favorites');
+});
+
+//Deregisters an existing user; returns a confirmation message that the user has been removed.//
+app.delete('/users/:username', (req, res) => {
+    res.send('This will delete a user');
 });
 
 app.get('/', (req,res) => {
